b0605a3dd0088ad31c59350946b3fa556afc870a
riverdev <yariv.rivercybe@gmail.com>
2022-11-07 09:12:41 +0200

    Added RxJs operator for filtering the value of the form to just the VALID value and also added a delay operator so that user can see what they typed before the form displays a new equation

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { delay, filter } from 'rxjs';
 import { MathValidator } from '../math-validator';
 
 @Component({
@@ -10,7 +11,39 @@ import { MathValidator } from '../math-validator';
 export class EquationComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.mathForm.statusChanges
+      .pipe(
+        filter( value => value === 'VALID'),
+        delay(200)
+      )
+      .subscribe( () => {
+
+    //* This is another option to verify if the form is valid, it uses an if-statement
+    // instead of the 'filter()' operator.
+    // this.mathForm.statusChanges.pipe((delay(200))).subscribe((value) => {
+    //   if (value === 'INVALID') {
+    //     return; //If wrong answer then ignore
+    //   }
+
+      //From this line downward is the case where user entered a correct answer
+
+      //(1) Generate new random numbers and emoty the Answer field to reset equation
+      //*This is the long-way to set multiple values:
+      // this.mathForm.controls.fieldA.setValue(this.generateRandomNumber());
+      // this.mathForm.controls.fieldB.setValue(this.generateRandomNumber());
+      // this.mathForm.controls.fieldAnswer.setValue('');
+
+      //*This is the long-way to set multiple values:
+      this.mathForm.setValue({
+        fieldA: this.generateRandomNumber(),
+        fieldB: this.generateRandomNumber(),
+        fieldAnswer: '',
+      });
+
+    });
+  }
 
   mathForm = new FormGroup(
     {
@@ -18,7 +51,7 @@ export class EquationComponent implements OnInit {
       fieldB: new FormControl(this.generateRandomNumber()),
       fieldAnswer: new FormControl(''),
     },
-    [ MathValidator.addition('fieldAnswer', 'fieldA', 'fieldB'),  ]
+    [MathValidator.addition('fieldAnswer', 'fieldA', 'fieldB')]
   );
 
   get fieldA() {
